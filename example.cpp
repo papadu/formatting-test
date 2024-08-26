b9390910926e0a6e649e17229c854f8316b265ed
Michael Brown <papadu@users.noreply.github.com>
2024-08-26 12:39:01 +0100

    bad formatting

--- example.cpp
+++ example.cpp
@@ -1,7 +1,7 @@
-#include "example.hpp"
-#include <iostream>
-void Example::set_value( int v ) { value=v;}
-int Example::get_value( ) { return value; }
-void Example::print_value() {std::cout<<"Value: "<<value<<std::endl; }
-int main ( ) {Example e;e.set_value(42);
-e.print_value();return 0; }
+    #include "example.hpp"
+    #include <iostream>
+    void Example::set_value( int v ) { value=v;}
+    int Example::get_value( ) { return value; }
+    void Example::print_value() {std::cout<<"Value: "<<value<<std::endl; }
+    int main ( ) {Example e;e.set_value(42);
+    e.print_value();return 0; }
