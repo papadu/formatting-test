3181aa944db84bc02682efbd12c8c639a087cefb
Michael Brown <michael.brown@bromptontech.com>
2024-08-30 20:34:49 +0100

    sss

--- example.cpp
+++ example.cpp
@@ -2,7 +2,7 @@
             #include <iostream>
             void Example::set_value( int v ) { value=v;}
             int Example::get_value( ) { return value; }
-            void Example::print_value() {std::cout<<"Value: "<<value<<std::endl; }
+                    void Example::print_value() {std::cout<<"Value: "<<value<<std::endl; }
             int main ( ) {Example e;e.set_value(42);
             int main ( ) {Example e;e.set_value(42);
             e.print_value();return 0; }
